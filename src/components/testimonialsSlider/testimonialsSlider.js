f5559816716cf0b215324ebb6ed7a9515f91b35a
Kiryu <13matheusilva@gmail.com>
2024-04-04 16:03:34 -0300

    projeto iniciado

---
+++ src/components/testimonialsSlider/testimonialsSlider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestimonialsSlider = () => {
+    return(
+        <main className="container">
+            
+        </main>
+    )
+}
+
+export default TestimonialsSlider
