cc1d3f497cfba008917c7a111b465c5d95dd1143
Kiryu <13matheusilva@gmail.com>
2024-04-04 16:21:26 -0300

    tanya testimonial component add

--- src/components/testimonialsSlider/testimonialsSlider.js
+++ src/components/testimonialsSlider/testimonialsSlider.js
@@ -1,9 +1,10 @@
 import React from "react";
+import TestimonialsTanya from "../testimonials/testimonialsTanya/testimonialsTanya";
 
 const TestimonialsSlider = () => {
     return(
         <main className="container">
-            
+            <TestimonialsTanya />
         </main>
     )
 }
